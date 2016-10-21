964c2c81f611068e11efb6642398714adb3ca48e
300872772 <mamun_131@hotmail.com>
2016-10-21 01:38:20 -0400

    done

---
+++ COMP305-PlatformerGame/Assets/_Scripts/EnemyController.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+	// PRIVATE INSTANCE VARIABLES 
+	private Transform _transform;
+	private Rigidbody2D _rigidbody;
+	private bool _isGrounded;
+	private bool _isGroundAhead;
+	private bool _isPillarAhead;
+	private bool _isPlayerDetected;
+	private int _angryMovementCount;
+
+	// PUBLIC INSTANCE VARIABLES (FOR TESTING)
+	public float Speed = 5f;
+	public float MaximumSpeed = 4f;
+	public Transform SightStart;
+	public Transform SightEnd;
+	public Transform LineOfSight;
+
+
+	// Use this for initialization
+	void Start () {
+		// make a reference to this object's Transform and Rigidbody2D components
+		this._transform = GetComponent<Transform> ();
+		this._rigidbody = GetComponent<Rigidbody2D> ();
+		this._isGrounded = false;
+		this._isGroundAhead = true;
+		this._isPlayerDetected = false;
+		this._isPillarAhead = false;
+	}
+	
+	// Update is called once per frame (Physics)
+	void FixedUpdate () {
+		// check if the object is grounded 
+		if (this._isGrounded) {
+			// move the object in the direction of his local scale
+			this._rigidbody.velocity = new Vector2(this._transform.localScale.x, 0) * this.Speed;
+
+			this._isGroundAhead = Physics2D.Linecast (
+				this.SightStart.position,
+				this.SightEnd.position,
+				1 << LayerMask.NameToLayer ("Solid"));
+			
+			this._isPillarAhead = Physics2D.Linecast (
+				this.SightStart.position,
+				this.SightEnd.position,
+				1 << LayerMask.NameToLayer ("Pillar"));
+
+			this._isPlayerDetected = Physics2D.Linecast (
+				this.SightStart.position,
+				this.LineOfSight.position,
+				1 << LayerMask.NameToLayer ("Player"));
+
+			// for debugging purposes only
+			Debug.DrawLine(this.SightStart.position, this.SightEnd.position);
+			Debug.DrawLine(this.SightStart.position, this.LineOfSight.position);
+
+			// check if there is ground ahead for the object to walk
+			if (this._isGroundAhead == false) {
+				// flip the direction
+				this._flip();
+				_angryMovementCount += 1;
+			}
+			if (this._isPillarAhead == true) {
+				// flip the direction
+				this._flip();
+				_angryMovementCount += 1;
+			}
+			// check if player is detected and then increase speed
+			if (this._isPlayerDetected) {
+				// increase speed to maximumSpeed
+				this.Speed *= 2;
+				if (this.Speed >= this.MaximumSpeed) {
+					this.Speed = this.MaximumSpeed;
+					_angryMovementCount = 0;
+				}
+			} else {
+				
+				if (_angryMovementCount > 2) {
+					this.Speed = 2f;
+				}
+			}
+		}
+
+	}
+
+	// object is colliding with another one of its kind - flip directions
+	private void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject.CompareTag ("Enemy")) {
+			this._flip ();
+		}	
+		if (other.gameObject.CompareTag ("Pillar")) {
+			this._isPillarAhead=true;
+		}	
+	}
+		
+	// object is grounded if it stays on the platform
+	private void OnCollisionStay2D(Collision2D other) {
+		if (other.gameObject.CompareTag ("Platform")) {
+			this._isGrounded = true;
+		}
+	}
+
+	// object is not grounded if it leaves the platform
+	private void OnCollisionExit2D(Collision2D other) {
+		if (other.gameObject.CompareTag ("Platform")) {
+			this._isGrounded = false;
+		}
+		if (other.gameObject.CompareTag ("Pillar")) {
+			this._isPillarAhead=false;
+		}
+	}
+
+	/**
+	 * This method flips the character's bitmap across the x-axis
+	 */
+	private void _flip () {
+		if (this._transform.localScale.x == 1) {
+			this._transform.localScale = new Vector2 (-1f, 1f);
+		} else {
+			this._transform.localScale = new Vector2 (1f, 1f);
+		}
+	}
+}
---
+++ COMP305-PlatformerGame/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,154 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	// PRIVATE INSTANCE VARIABLES
+	private Transform _transform;
+	private Rigidbody2D _rigidbody;
+	private Animator _animator;
+	private GameObject _camera;
+	private GameObject _spawnPoint;
+	private GameObject _gameControllerObject;
+	private GameController _gameController;
+
+	private float _move;
+	private float _jump;
+	private bool _isFacingRight;
+	private bool _isGrounded;
+
+	// PUBLIC INSTANCE VARIABLES (FOR TESTING)
+	public float Velocity = 10f;
+	public float JumpForce = 100f;
+
+
+	[Header("Sound Clips")]
+	public AudioSource JumpSound;
+	public AudioSource DeathSound;
+	public AudioSource CoinSound;
+	public AudioSource HurtSound;
+	public AudioSource EnemyDeadSound;
+
+	// Use this for initialization
+	void Start () {
+		this._initialize ();
+	}
+	
+	// Update is called once per frame (Physics)
+	void FixedUpdate () {
+
+		if (this._isGrounded) {
+			// check if input is present for movement
+			this._move = Input.GetAxis ("Horizontal");
+			if (this._move > 0f) {
+				// set the animation state to "Walk"
+				this._animator.SetInteger ("RS", 1);
+				this._move = 1;
+				this._isFacingRight = true;
+				this._flip ();
+			} else if (this._move < 0f) {
+				this._animator.SetInteger ("RS", 1);
+				this._move = -1;
+				this._isFacingRight = false;
+				this._flip ();
+			} else {
+				// set the animation state to "Idle"
+				this._animator.SetInteger ("RS", 0);
+				this._move = 0f;
+			}
+
+			// check if input is present for jumping
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				this._jump = 1f;
+				this.JumpSound.Play ();
+			}
+
+			this._rigidbody.AddForce (new Vector2 (
+				this._move * this.Velocity, 
+				this._jump * this.JumpForce), 
+				ForceMode2D.Force);
+
+		} else {
+			this._move = 0f;
+			this._jump = 0f;
+		}
+
+		this._camera.transform.position = new Vector3 (
+			this._transform.position.x, 
+			this._transform.position.y, 
+			-10f);
+
+	}
+
+	// PRIVATE METHODS
+	/**
+	 * This method initializes variables and object when called
+	 */
+	private void _initialize() {
+		this._transform = GetComponent<Transform> ();
+		this._rigidbody = GetComponent<Rigidbody2D> ();
+		this._animator = GetComponent<Animator> ();
+		this._camera = GameObject.FindWithTag ("MainCamera");
+		this._spawnPoint = GameObject.FindWithTag ("SpawnPoint");
+
+		this._gameControllerObject = GameObject.Find ("Game Controller");
+		this._gameController = this._gameControllerObject.GetComponent<GameController> () as GameController;
+
+		this._move = 0f;
+		this._isFacingRight = true;
+		this._isGrounded = false;
+	}
+
+	/**
+	 * This method flips the character's bitmap across the x-axis
+	 */
+	private void _flip () {
+		if (this._isFacingRight) {
+			this._transform.localScale = new Vector2 (1f, 1f);
+		} else {
+			this._transform.localScale = new Vector2 (-1f, 1f);
+		}
+	}
+
+	private void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject.CompareTag ("DeathPlane")) {
+			// move the player's position to the spawn point's position
+			this._transform.position = this._spawnPoint.transform.position;
+			this.DeathSound.Play ();
+			this._gameController.LivesValue -= 1;
+		}
+
+		if (other.gameObject.CompareTag ("Relic")) {
+			Destroy (other.gameObject);
+			this.CoinSound.Play ();
+			this._gameController.ScoreValue += 100;
+		}
+
+		if (other.gameObject.CompareTag ("Enemy")) {
+			// move the player's position to the spawn point's position
+			this._transform.position = this._spawnPoint.transform.position;
+			this.HurtSound.Play ();
+			this._gameController.LivesValue -= 1;
+		}
+	}
+
+	private void OnCollisionStay2D(Collision2D other) {
+		if (other.gameObject.CompareTag ("Platform")) {
+			this._isGrounded = true;
+		}
+	}
+
+	private void OnCollisionExit2D(Collision2D other) {
+		this._animator.SetInteger ("HeroState", 2);
+
+		this._isGrounded = false;
+	}
+
+	// debug if player lands on object's head
+	private void OnTriggerEnter2D(Collider2D other) {
+		if(other.gameObject.CompareTag("Enemy")) {
+			this.EnemyDeadSound.Play ();
+			//Destroy (other.gameObject);
+			this._gameController.LivesValue -= 1;
+		}
+	}
+}
